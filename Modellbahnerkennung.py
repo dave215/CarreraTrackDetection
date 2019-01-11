c454369bda5ab3de627e3f0bf6a6576195c51a35
98SamM <45296543+98SamM@users.noreply.github.com>
2019-01-11 17:59:27 +0100

    Update Modellbahnerkennung.py

--- Modellbahnerkennung.py
+++ Modellbahnerkennung.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\samir\Documents\QtDesigner\GrobeMaske.ui'
+# Form implementation generated from reading ui file 'C:\Users\samir\Documents\QtDesigner\Modellbahnerkennung.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -8,73 +8,67 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Dialog1(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(2000,1410)
-
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(400, 630, 140, 50))
+class Ui_QMainWindow(object):
+    def setupUi(self, QMainWindow):
+        QMainWindow.setObjectName("QMainWindow")
+        QMainWindow.resize(2000, 1600)
+        self.centralwidget = QtWidgets.QWidget(QMainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(40, 380, 150, 60))
         self.pushButton.setObjectName("pushButton")
-
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(400, 1310, 140, 50))
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 140, 1660, 50))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setReadOnly(1)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(40, 40, 200, 60))
+        self.comboBox.setObjectName("comboBox")
+        icon = QtGui.QIcon.fromTheme("Kamera")
+        self.comboBox.addItem(icon, "")
+        icon = QtGui.QIcon.fromTheme("Bild")
+        self.comboBox.addItem(icon, "")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(1720, 140, 200, 50))
         self.pushButton_2.setObjectName("pushButton_2")
-
-        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_3.setGeometry(QtCore.QRect(1360, 630, 140, 50))
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 500, 1800, 1060))
+        self.label.setText("")
+#         self.label.setPixmap(QtGui.QPixmap("test2.jpg"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("ImgStrecke")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(1720, 250, 200, 50))
         self.pushButton_3.setObjectName("pushButton_3")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(40, 250, 1660, 50))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_2.setReadOnly(1)
+        QMainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(QMainWindow)
+        self.statusbar.setObjectName("statusbar")
+        QMainWindow.setStatusBar(self.statusbar)
 
-        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_4.setGeometry(QtCore.QRect(1360, 1310, 140, 50))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.retranslateUi(QMainWindow)
+        QtCore.QMetaObject.connectSlotsByName(QMainWindow)
+        QMainWindow.setTabOrder(self.comboBox, self.lineEdit)
+        QMainWindow.setTabOrder(self.lineEdit, self.pushButton)
 
-        self.label60 = QtWidgets.QLabel(Dialog)
-        self.label60.setGeometry(QtCore.QRect(40, 50, 900, 530))
-        self.label60.setText("")
-        self.label60.setScaledContents(True)
-        self.label60.setObjectName("Mask60")
-
-        self.label80 = QtWidgets.QLabel(Dialog)
-        self.label80.setGeometry(QtCore.QRect(40, 730, 900, 530))
-        self.label80.setText("")
-        self.label80.setScaledContents(True)
-        self.label80.setObjectName("Mask80")
-
-        self.label100 = QtWidgets.QLabel(Dialog)
-        self.label100.setGeometry(QtCore.QRect(980, 50, 900, 530))
-        self.label100.setText("")
-        self.label100.setScaledContents(True)
-        self.label100.setObjectName("Mask100")
-
-        self.label120 = QtWidgets.QLabel(Dialog)
-        self.label120.setGeometry(QtCore.QRect(980, 730, 900, 530))
-        self.label120.setText("")
-        self.label120.setScaledContents(True)
-        self.label120.setObjectName("Mask120")
-
-        # self.Mask120 = QtWidgets.QGraphicsView(Dialog)
-        # self.Mask120.setGeometry(QtCore.QRect(400, 280, 321, 221))
-        # self.Mask120.setObjectName("Mask120")
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, QMainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Grobe Maske"))
-        self.pushButton.setText(_translate("Dialog", "60"))
-        self.pushButton_2.setText(_translate("Dialog", "80"))
-        self.pushButton_3.setText(_translate("Dialog", "100"))
-        self.pushButton_4.setText(_translate("Dialog", "120"))
+        QMainWindow.setWindowTitle(_translate("QMainWindow", "Modellbahnerkennung"))
+        self.pushButton.setText(_translate("QMainWindow", "Start"))
+        self.comboBox.setItemText(0, _translate("QMainWindow", "Kamera"))
+        self.comboBox.setItemText(1, _translate("QMainWindow", "Bild"))
+        self.pushButton_2.setText(_translate("QMainWindow", "Bild wählen"))
+        self.pushButton_3.setText(_translate("QMainWindow", "Strecke speichern"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog1()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    QMainWindow = QtWidgets.QMainWindow()
+    ui = Ui_QMainWindow()
+    ui.setupUi(QMainWindow)
+    QMainWindow.show()
     sys.exit(app.exec_())
-
